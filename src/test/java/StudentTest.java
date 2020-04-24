62d9f489fbe24baff518e59d1f92ec77ba38bfe1
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-23 20:20:56 -0500

    updatingTree

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//public class StudentTest {
+//    @Test
+//    public void testCreateStudent(){
+//        Student fer = new Student(1L, "fer");
+//        Student ryan = null;
+//        assertNull(ryan);
+//        assertNotNull(fer);
+//    }
